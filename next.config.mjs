c2a01a9810edf71f07dc9445c39e7ce01b4c9c14
Ramkumarrja <ramkumarraja240@gmail.com>
2025-12-06 09:57:09 +0530

    updated code .

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,42 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**.cloudinary.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'share.google',
+      },
+      {
+        protocol: 'https',
+        hostname: 'share.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.google.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.amazonaws.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.imgur.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.pexels.com',
+      },
     ],
   },
 }
